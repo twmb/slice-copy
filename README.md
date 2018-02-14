9acba493b499e2f1411e66d6979036e564840891
Travis Bischel <twmb@lavabit.com>
2018-02-14 13:07:25 -0800

    remove u8s_copy, add specialization for copy on nightly, update docs

--- README.md
+++ README.md
@@ -4,5 +4,17 @@ slice-copy
 [![Build Status](https://travis-ci.org/twmb/slice-copy.svg?branch=master)](https://travis-ci.org/twmb/slice-copy)  [![Crates.io](https://img.shields.io/crates/v/slice-copy.svg)](https://crates.io/crates/slice-copy) [![Documentation](https://docs.rs/slice-copy/badge.svg)](https://docs.rs/slice-copy/)
 
 Go style copying for slices. For times where you would rather use the amount
-copied to adjust your slices as opposed to adjusting your slices to determine
-the amount to copy.
+copied to adjust your slices as opposed to determining the amount to copy,
+adjusting your slices, and finally copying.
+
+```rust
+use slice_copy::copy;
+
+let mut l = b"hello".to_vec();
+let r = b"goodbye".to_vec();
+
+let n = copy(&mut l, &r);
+
+assert_eq!(n, 5);
+assert_eq!(l, b"goodb");
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
+#![cfg_attr(feature = "nightly", feature(specialization))]
+
 //! This crate provides Go style copying / cloning for slices.
 //!
-//! For times when you do not want to worry about slices being unequal sizes, this crate is here
-//! for you.
+//! This crate is for those times where it is easier to adjust slices based off the number of
+//! elements copied, as opposed to determining the amount to copy before adjusting slices and
+//! finally copying.
 //!
 //! # Examples
-//! 
+//!
 //! We can use `copy` for types that implement `Copy`.
 //!
 //! ```
 //! use slice_copy::copy;
-//! 
+//!
 //! let mut l = b"hello".to_vec();
 //! let r = b"goodbye".to_vec();
 //!
@@ -19,25 +22,11 @@
 //! assert_eq!(l, b"goodb");
 //! ```
 //!
-//! `u8`s are special cased for if either side is short.
-//!
-//! ```
-//! use slice_copy::u8s_copy;
-//! 
-//! let mut l = vec![1 as u8, 2, 3];
-//! let r = vec![10 as u8, 11, 12, 13, 14];
-//!
-//! let n = u8s_copy(&mut l, &r);
-//!
-//! assert_eq!(n, 3);
-//! assert_eq!(l, vec![10 as u8, 11, 12]);
-//! ```
-//!
-//! Similar to `copy`, we can use `clone`.
+//! Similarly, we can use `clone` for types that implement `Clone`.
 //!
 //! ```
 //! use slice_copy::clone;
-//! 
+//!
 //! let mut l = b"foobarbaz".to_vec();
 //! let r = b"biz".to_vec();
 //!
@@ -49,37 +38,99 @@
 
 use std::cmp::min;
 
-/// Copies as many `u8` as possible from `src` into `dst`, returning the number of `u8` copied.
-/// This function is a special case of [`copy`]; `u8` slices have a special optimization for
-/// reading if they are of length 1.
-///
-/// [`copy`]: fn.copy.html
-#[inline]
-pub fn u8s_copy(dst: &mut [u8], src: &[u8]) -> usize {
-    // Read for &[u8] special cases 1-length slices.
-    use std::io::Read;
-    let len = min(src.len(), dst.len());
-    (&src[..len]).read(&mut dst[..len]).expect("&[u8] reads never error")
+#[cfg(feature = "nightly")]
+trait Cpy<T = Self>
+where
+    T: ?Sized,
+{
+    fn copy(&mut self, src: &T) -> usize;
+}
+
+#[cfg(feature = "nightly")]
+default impl<T> Cpy<[T]> for [T]
+where
+    T: Copy,
+{
+    #[inline]
+    fn copy(&mut self, src: &Self) -> usize {
+        let len = min(src.len(), self.len());
+        (&mut self[..len]).copy_from_slice(&src[..len]);
+        len
+    }
+}
+
+#[cfg(feature = "nightly")]
+impl Cpy<[u8]> for [u8] {
+    #[inline]
+    fn copy(&mut self, src: &Self) -> usize {
+        use std::io::Read;
+        let len = min(src.len(), self.len());
+        (&src[..len])
+            .read(&mut self[..len])
+            .expect("&[u8] reads never error")
+    }
 }
 
 /// Copies as many `T` as possible from `src` into `dst`, returning the number of `T` copied. This
 /// function is short form for `dst.copy_from_slice(src)`, but accounts for if their lengths are
 /// unequal to avoid panics.
+///
+/// With the `nightly` feature, `[u8]` is specialized to use [`Read`], which is implemented
+/// specially for small slices.
+///
+/// [`Read`]: https://doc.rust-lang.org/std/primitive.slice.html#impl-Read
+///
+/// # Examples
+///
+/// ```
+/// use slice_copy::copy;
+///
+/// let mut l = vec![1 as u8, 2, 3, 4, 5];
+/// let r = vec![10, 11, 12];
+///
+/// let n = copy(&mut l, &r);
+///
+/// assert_eq!(n, 3);
+/// assert_eq!(l, vec![10, 11, 12, 4, 5]);
+/// ```
 #[inline]
-pub fn copy<T>(dst: &mut[T], src: &[T]) -> usize
-where T: Copy
+pub fn copy<T>(dst: &mut [T], src: &[T]) -> usize
+where
+    T: Copy,
 {
-    let len = min(src.len(), dst.len());
-    (&mut dst[..len]).copy_from_slice(&src[..len]);
-    len
+    #[cfg(feature = "nightly")]
+    {
+        dst.copy(src)
+    }
+    #[cfg(not(feature = "nightly"))]
+    {
+        let len = min(src.len(), dst.len());
+        (&mut dst[..len]).copy_from_slice(&src[..len]);
+        len
+    }
 }
 
 /// Clones as many `T` as possible from `src` into `dst`, returning the number of `T` cloned. This
 /// function is short form for `dst.clone_from_slice(src)`, but accounts for if their lengths are
 /// unequal to avoid panics.
+///
+/// Examples
+///
+/// ```
+/// use slice_copy::clone;
+///
+/// let mut l = b"left".to_vec();
+/// let r = b"right".to_vec();
+///
+/// let n = clone(&mut l, &r);
+///
+/// assert_eq!(n, 4);
+/// assert_eq!(l, b"righ");
+/// ```
 #[inline]
-pub fn clone<T>(dst: &mut[T], src: &[T]) -> usize
-where T: Clone
+pub fn clone<T>(dst: &mut [T], src: &[T]) -> usize
+where
+    T: Clone,
 {
     let len = min(src.len(), dst.len());
     (&mut dst[..len]).clone_from_slice(&src[..len]);
@@ -94,25 +145,24 @@ fn test_copy() {
 
     // longer to shorter
     let (mut l, r) = lr();
-    assert_eq!(u8s_copy(&mut l, &r), 5);
+    assert_eq!(copy(&mut l, &r), 5);
     assert_eq!(l, b"goodb");
     assert_eq!(r, b"goodbye");
 
     // shorter to longer
     let (l, mut r) = lr();
-    assert_eq!(u8s_copy(&mut r, &l[..4]), 4);
+    assert_eq!(copy(&mut r, &l[..4]), 4);
     assert_eq!(l, b"hello");
     assert_eq!(r, b"hellbye");
 
     // dst length 0
     let (mut l, r) = lr();
-    assert_eq!(u8s_copy(&mut l[..0], &r), 0);
+    assert_eq!(copy(&mut l[..0], &r), 0);
     assert_eq!(l, b"hello");
     assert_eq!(r, b"goodbye");
 
     // src length 0
-    assert_eq!(u8s_copy(&mut l, &r[..0]), 0);
+    assert_eq!(copy(&mut l, &r[..0]), 0);
     assert_eq!(l, b"hello");
     assert_eq!(r, b"goodbye");
 }
-
